23ccbb8a340d9b064c71174d8c52592b17558688
Edwin Rodríguez <edwrodrig@gmail.com>
2017-12-06 16:20:20 -0300

    Changes

--- scripts/minify.php
+++ scripts/minify.php
@@ -6,5 +6,6 @@ $minifier = new \MatthiasMullie\Minify\JS;
 $minifier->add(__DIR__ . '/../js/form.js');
 $minifier->add(__DIR__ . '/../js/request.js');
 
+$minifier->minify(__DIR__ . '/../js/request.min.js');
 $minifier->gzip(__DIR__ . '/../js/request.min.js.gz');
 
